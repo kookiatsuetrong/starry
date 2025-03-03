8c89c5753e03c745f58900a07e86d966b0bb2e40
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2025-03-03 23:50:39 +0700

    Add dynamically loading

--- README.md
+++ README.md
@@ -289,6 +289,32 @@ Modal Dialog
 	}
 ```
 
+Refreshing Dynamically
+```java
+import starry.StarryApp;
+import java.io.FileReader;
+
+public class Start extends StarryApp {
+	
+	public void main() {
+		String buffer = "";
+		try {		
+			FileReader fr = new FileReader("code/main.html");
+			while (true) {
+				int k = fr.read();
+				if (k < 0) break;
+				buffer += (char)k;
+			}
+		} catch (Exception e) { }
+		loadContent(buffer);
+	}
+	
+	public void setup() {
+		setAction("refresh", e -> main() );
+	}
+}
+```
+
 
 Common Methods
 ```java
--- code/Start.java
+++ code/Start.java
@@ -1,76 +1,22 @@
-
 import starry.StarryApp;
+import java.io.FileReader;
 
 public class Start extends StarryApp {
 	
 	public void main() {
-		loadContent(mainPage + style);
+		String buffer = "";
+		try {		
+			FileReader fr = new FileReader("code/main.html");
+			while (true) {
+				int k = fr.read();
+				if (k < 0) break;
+				buffer += (char)k;
+			}
+		} catch (Exception e) { }
+		loadContent(buffer);
 	}
 	
 	public void setup() {
-		setAction("settings-button", e -> showSettings() );
-		setAction("home-button", e -> showHome() );
+		setAction("refresh", e -> main() );
 	}
-	
-	void showSettings() {
-		loadContent(settingsPage + style);
-	}
-	
-	void showHome() {
-		loadContent(mainPage + style);
-	}
-	
-String mainPage = 
-"""
-<body class="white">
-	<button id="settings-button">Settings</button>
-</body>
-""";
-
-String settingsPage = 
-"""
-<body class="light">
-	<button id="home-button">Home</button>
-</body>
-""";
-
-String style = 
-"""
-<style>
-* {
-	outline: none;
-	font-family: sans-serif;
 }
-body {
-	padding: 1rem;
-}
-body.white {
-	background: white;
-}
-body.light {
-	background: #f0f4ff;
-}
-button {
-	color: white;
-	background: dodgerblue;
-	padding: .5rem 1rem;
-	border: none;
-	border-radius: .35rem;
-	transition: background .25s linear;
-	font-size: 1rem;
-}
-button:hover {
-	background: steelblue;
-}
-input {
-	border: none;
-	border-radius: .35rem;
-	padding: .5rem .5rem;
-	background: #eee;
-	font-size: 1rem;
-}
-</style>
-""";
-
-}
-
