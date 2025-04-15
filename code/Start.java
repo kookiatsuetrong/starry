937d70756c63035bd3ae5b59e4a4d4816c671dce
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2025-04-15 07:25:51 +0700

    Fix moving by dragging border

--- code/Start.java
+++ code/Start.java
@@ -1,8 +1,13 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.geom.RoundRectangle2D;
+
 import java.io.InputStream;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
@@ -16,6 +21,7 @@ import javax.swing.WindowConstants;
 
 import starry.StarryApp;
 import org.w3c.dom.events.Event;
+// import org.w3c.dom.events.MouseEvent;
 
 public class Start {
 	public static void main(String[] data) {
@@ -28,6 +34,7 @@ class Starry {
 	WebView page;
 	public static int WIDTH  = 480;
 	public static int HEIGHT = 360;
+	public static JFrame frame;
 	
 	Starry(String file) {
 		this();
@@ -35,7 +42,7 @@ class Starry {
 	}
 	
 	Starry() {
-		JFrame frame = new JFrame("");
+		frame = new JFrame("");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setUndecorated(true);
 		frame.setBackground(new java.awt.Color(0,0,0,0));
@@ -143,7 +150,14 @@ class RoundRectangle extends JPanel {
 }
 
 class Outer extends JPanel {
+	Outer() {
+		super();
+		addMouseListener(new Sample());
+		addMouseMotionListener(new SampleMotion());
+	}
 	
+	java.awt.Color moving  = new java.awt.Color(0,0,0,220);
+	java.awt.Color display = new java.awt.Color(0xF0, 0xF4, 0xFF, 220);
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D)g;
@@ -151,9 +165,43 @@ class Outer extends JPanel {
 		g2d.setRenderingHint(
 			RenderingHints.KEY_ANTIALIASING,
 			RenderingHints.VALUE_ANTIALIAS_ON);
-		g2d.setColor(new java.awt.Color(0xF0, 0xF4, 0xFF, 220) );
+		g2d.setColor( SampleMotion.start == null ? display : moving );
 		g2d.fillRoundRect(1, 1, Starry.WIDTH-2, Starry.HEIGHT-2, 25, 25);
 	}
+}
+
+class SampleMotion extends MouseMotionAdapter {
+	
+	public static Point start;
+	
+	@Override
+	public void mouseDragged(java.awt.event.MouseEvent e) {
+		int x = e.getXOnScreen();
+		int y = e.getYOnScreen();
+		Starry.frame.setLocation(x - start.x, y - start.y);
+	}
+
+}
+
+class Sample extends MouseAdapter {
+	
+	@Override
+	public void mousePressed(java.awt.event.MouseEvent e) {
+		int x = e.getXOnScreen();
+		int y = e.getYOnScreen();
+		
+		Point framePoint = Starry.frame.getLocationOnScreen();
+		int dx = x - framePoint.x;
+		int dy = y - framePoint.y;
+		
+		SampleMotion.start = new Point(dx, dy);
+	}
+	
+	@Override
+	public void mouseReleased(java.awt.event.MouseEvent e) {
+		SampleMotion.start = null;
+	}
+	
 }
 
 
